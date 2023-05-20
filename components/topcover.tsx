611eb99d64e5745dd78a46d60c4764422a1c6989
Facundo Arias <facundolautaroarias@gmail.com>
2023-05-20 09:55:59 -0300

    background hero keyframes added

--- components/topcover.tsx
+++ components/topcover.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import styles from "../styles/TopCover.module.scss";
+import Image from "next/image";
 const { motion } = require("framer-motion");
+import bg1 from "../public/assets/img011.jpg";
 
 const TopCover = () => {
   return (
     <div className={styles.topcover_container}>
       <motion.div
         className={styles.topcover_image}
-        initial={{ opacity: 0, color: '#fff' }}
+        initial={{ opacity: 0, color: "#fff" }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.1 }}
       >
-        <motion.h1>cool img goes here</motion.h1>
+        {/* <motion.h1></motion.h1> */}
+        {/* <div>something</div> */}
+        <div className={styles.actor}></div>
+        <div className={styles.actor}></div>
+        <div className={styles.actor}></div>
+        <div className={styles.actor}></div>
+
       </motion.div>
     </div>
   );
